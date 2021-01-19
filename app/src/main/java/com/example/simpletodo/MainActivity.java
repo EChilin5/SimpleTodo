88c8ec56f9de5c8cef0b9ed3c4bf55badeebe0bc
EChilin5 <edgar3ac@gmail.com>
2021-01-19 15:20:29 -0800

    stretch goals, update individual item

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,9 +1,13 @@
-package com.example.simpletodo;
+ package com.example.simpletodo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +24,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_ITEM_TEXT = "item.text";
+    public static final String KEY_ITEM_POSITION = "item.position";
+    public static final int EDIT_TEXT_CODE = 20;
 
     List<String> items;
 
@@ -51,7 +59,21 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        itemsAdapter = new ItemsAdapter(items, OnLongClickListener);
+        ItemsAdapter.OnClickListener OnClickListener = new ItemsAdapter.OnClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                Log.d("Main Activity", "Single clicked at postion " + position);
+                // create the new activity
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                // pass the data being edited
+                i.putExtra(KEY_ITEM_TEXT, items.get(position));
+                i.putExtra(KEY_ITEM_POSITION, position);
+                //display the activity
+                startActivityForResult(i, EDIT_TEXT_CODE);
+
+            }
+        };
+        itemsAdapter = new ItemsAdapter(items, OnLongClickListener, OnClickListener);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
 
@@ -69,6 +91,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //handle the result of the edit activity
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK && requestCode == EDIT_TEXT_CODE){
+            // retrieve teh updated text value
+            String itemText = data.getStringExtra(KEY_ITEM_TEXT);
+
+            //extract the original posiotion of the edited position k
+            int position  = data.getExtras().getInt(KEY_ITEM_POSITION);
+
+            //update the model at the right width new item
+            items.set(position, itemText);
+            //notify the adpater
+            itemsAdapter.notifyItemChanged(position);
+            //presist the changes
+            saveItems();
+            Toast.makeText(getApplicationContext(), "iTEM UPDATED SUCCESSFULLY", Toast.LENGTH_SHORT ).show();
+
+
+        }else{
+            Log.w("MainActivity", "Unkown call to onActivityResult");
+        }
+    }
+
     private File getDataFile(){
         return new File(getFilesDir(),"data.txt");
     }
